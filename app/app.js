df6e81cfb1183d11933f19545697102cb2a650ef
StanislavZaborkin <stanislav.zaborkin@gmail.com>
2017-12-26 18:33:49 +0200

    added smth

--- app/app.js
+++ app/app.js
@@ -9,37 +9,67 @@ var myApp = angular.module('myApp', [
     $scope.users = [];
     $scope.filterArr = [];
 
-    $scope.getFunc = function () {
+    $scope.getCurrency = function () {
+        var clone = [];
+        for (var key in $scope.users) {
+            clone = $scope.users[key].exchangeRate;
+        }
+        $scope.newArr = [];
+        for (var i = 0; i < clone.length-1; i++) {
+            if (clone[i].currency === 'EUR' || clone[i].currency === 'USD'
+                || clone[i].currency === 'GBP' || clone[i].currency ===  'CAD') {
+                $scope.newArr.push(clone[i]);
+            }
+        }
+        console.log($scope.newArr[1].saleRateNB);
+    };
 
-        /*var yesterday = new Date();
-        yesterday = yesterday.getDate() - 1;
-        $scope.url = 'https://api.privatbank.ua/p24api/exchange_rates?json&date=' + yesterday;*/
+    $scope.selectCur = function (val) {
+
+    }
+
+    $scope.getTodayRate = function (selCur) {
+        var today = new Date();
+        var urla = 'https://api.privatbank.ua/p24api/exchange_rates?json&date=' + (today.getDate()-5) + '.' +
+            (today.getMonth() + 1) + '.' + today.getFullYear();
 
         $http({
             method: 'GET',
-            url: 'https://api.privatbank.ua/p24api/exchange_rates?json&date=5.12.2017'
+            url: urla
         }).then(function successCallback(response) {
             $scope.users.push(response.data);
+            $scope.getCurrency();
+            $scope.labels = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-            /*for (var key in response.data){
-                if (key === 'exchangeRate'){
-                    $scope.filterArr.push(key);
-                }
-            }*/
-            $scope.users.forEach(function(item, i, arr) {
-                console.log( i + ": " + item + " (массив:" + arr + ")" );
-            });
+            $scope.series = [$scope.newArr[1].currency];
+            $scope.data = [
+                [$scope.newArr[1].saleRateNB]
+            ];
         }, function errorCallback(response) {
             console.log('error' + response.statusText);
         });
     };
 
-    $scope.getTodayRate = function () {
-
-    };
-
     $scope.getWeekRate = function () {
+        var today = new Date();
+        var urla = 'https://api.privatbank.ua/p24api/exchange_rates?json&date=' + (today.getDate()-5) + '.' +
+            (today.getMonth() + 1) + '.' + today.getFullYear();
 
+        $http({
+            method: 'GET',
+            url: urla
+        }).then(function successCallback(response) {
+            $scope.users.push(response.data);
+            $scope.getCurrency();
+            $scope.labels = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+
+            $scope.series = [$scope.newArr[1].currency];
+            $scope.data = [
+                [$scope.newArr[1].saleRateNB]
+            ];
+        }, function errorCallback(response) {
+            console.log('error' + response.statusText);
+        });
     };
 
     $scope.getMonthRate = function () {
@@ -47,7 +77,7 @@ var myApp = angular.module('myApp', [
     };
 
     $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
-    $scope.series = ['USD', 'UA'];
+    $scope.series = ['USD', 'UAH'];
     $scope.data = [
         [65, 59, 80, 81, 56, 55, 40],
         [28, 48, 40, 19, 86, 27, 90]
