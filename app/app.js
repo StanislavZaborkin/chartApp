35ea04928cac3b3d0b8fe27ec5b9e916edb438fb
StanislavZaborkin <StanislavZaborkin@gmail.com>
2017-12-27 17:32:26 +0200

    added day chart

--- app/app.js
+++ app/app.js
@@ -9,6 +9,13 @@ var myApp = angular.module('myApp', [
     $scope.users = [];
     $scope.filterArr = [];
 
+    $scope.checkboxModel = {
+        value1 : false,
+        value2 : false,
+        value3 : false,
+        value4 : false
+    };
+
     $scope.getCurrency = function () {
         var clone = [];
         for (var key in $scope.users) {
@@ -21,55 +28,108 @@ var myApp = angular.module('myApp', [
                 $scope.newArr.push(clone[i]);
             }
         }
-        console.log($scope.newArr[1].saleRateNB);
     };
 
-    $scope.selectCur = function (val) {
+    $scope.selectCur = function (i, j, x, y) {
+        if (j === undefined){
+            $scope.series = [$scope.newArr[i].currency];
+            $scope.data = [
+                [$scope.newArr[i].saleRateNB]
+            ];
+        }
+        else if (x === undefined) {
+            $scope.series = [$scope.newArr[i].currency, $scope.newArr[j].currency];
+            $scope.data = [
+                [$scope.newArr[i].saleRateNB],
+                [$scope.newArr[j].saleRateNB]
+            ];
+        }
+        else if (y === undefined) {
+            $scope.series = [$scope.newArr[i].currency, $scope.newArr[j].currency,
+                $scope.newArr[x].currency];
+            $scope.data = [
+                [$scope.newArr[i].saleRateNB],
+                [$scope.newArr[j].saleRateNB],
+                [$scope.newArr[x].saleRateNB]
+            ];
+        }
+        else if (y) {
+            $scope.series = [$scope.newArr[i].currency, $scope.newArr[j].currency,
+                $scope.newArr[x].currency, $scope.newArr[y].currency];
+            $scope.data = [
+                [$scope.newArr[i].saleRateNB],
+                [$scope.newArr[j].saleRateNB],
+                [$scope.newArr[x].saleRateNB],
+                [$scope.newArr[y].saleRateNB]
+            ];
+        }
+    };
 
-    }
-
-    $scope.getTodayRate = function (selCur) {
-        var today = new Date();
-        var urla = 'https://api.privatbank.ua/p24api/exchange_rates?json&date=' + (today.getDate()-5) + '.' +
-            (today.getMonth() + 1) + '.' + today.getFullYear();
-
+    $scope.getTodayRate = function () {
         $http({
             method: 'GET',
-            url: urla
+            url: 'https://api.privatbank.ua/p24api/exchange_rates?json&date=21.12.2017'
         }).then(function successCallback(response) {
             $scope.users.push(response.data);
             $scope.getCurrency();
-            $scope.labels = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
-            $scope.series = [$scope.newArr[1].currency];
-            $scope.data = [
-                [$scope.newArr[1].saleRateNB]
-            ];
+            if ($scope.checkboxModel.value1 && $scope.checkboxModel.value2
+                && $scope.checkboxModel.value3 && $scope.checkboxModel.value4) {
+                $scope.selectCur(0, 1, 2, 3);
+            }
+            else if ($scope.checkboxModel.value1 === true && $scope.checkboxModel.value2 === true
+                && $scope.checkboxModel.value3 === true) {
+                $scope.selectCur(0, 1, 2);
+            }
+            else if ($scope.checkboxModel.value1 === true && $scope.checkboxModel.value2 === true) {
+                $scope.selectCur(0, 1);
+            }
+            else if ($scope.checkboxModel.value1) {
+                $scope.selectCur(0);
+            }
+            else if ($scope.checkboxModel.value2) {
+                $scope.selectCur(1);
+            }
+            else if ($scope.checkboxModel.value3) {
+                $scope.selectCur(2);
+            }
+            else if ($scope.checkboxModel.value4) {
+                $scope.selectCur(3);
+            }
+            $scope.labels = ["Today"];
         }, function errorCallback(response) {
             console.log('error' + response.statusText);
         });
     };
 
     $scope.getWeekRate = function () {
-        var today = new Date();
-        var urla = 'https://api.privatbank.ua/p24api/exchange_rates?json&date=' + (today.getDate()-5) + '.' +
-            (today.getMonth() + 1) + '.' + today.getFullYear();
+        $scope.weekRate = [];
 
-        $http({
-            method: 'GET',
-            url: urla
-        }).then(function successCallback(response) {
-            $scope.users.push(response.data);
-            $scope.getCurrency();
-            $scope.labels = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+            $scope.getData = function () {
+                var urla = 'https://api.privatbank.ua/p24api/exchange_rates?json&date=' + $scope.i + '.12.2017';
+                $http({
+                    method: 'GET',
+                    url: urla
+                }).then(function successCallback(response) {
+                    $scope.users.push(response.data);
+                    $scope.getCurrency();
+                    $scope.weekRate.push($scope.newArr[0].saleRateNB);
+                    console.log($scope.weekRate);
+                }, function errorCallback(response) {
+                    console.log('error' + response.statusText);
+                });
+            };
+        for ($scope.i = 21; $scope.i < 15; $scope.i++){
+            setTimeout($scope.getData, 1000);
+        }
 
-            $scope.series = [$scope.newArr[1].currency];
-            $scope.data = [
-                [$scope.newArr[1].saleRateNB]
-            ];
-        }, function errorCallback(response) {
-            console.log('error' + response.statusText);
-        });
+
+        /*$scope.getCurrency();
+        $scope.series = [$scope.newArr[0].currency];
+        $scope.data = [
+            [$scope.weekRate]
+        ];*/
+        $scope.labels = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
     };
 
     $scope.getMonthRate = function () {
@@ -82,7 +142,7 @@ var myApp = angular.module('myApp', [
         [65, 59, 80, 81, 56, 55, 40],
         [28, 48, 40, 19, 86, 27, 90]
     ];
-    $scope.onClick = function (points, evt) {
+    /*$scope.onClick = function (points, evt) {
         console.log(points, evt);
     };
     $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
@@ -103,6 +163,6 @@ var myApp = angular.module('myApp', [
                 }
             ]
         }
-    };
+    };*/
 }]);
 
